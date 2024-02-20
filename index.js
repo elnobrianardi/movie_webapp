8ef299862f470f772c93a1839cd43258b6c0385e
elnobrianardi <142206280+elnobrianardi@users.noreply.github.com>
2024-02-20 17:53:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+import ReviewsDAO from './dao/reviewsDAO.js';
+
+const MongoClient =mongodb.MongoClient;
+const mongo_username = 
+consy mongo_password =
+const uri =
+`mongodb+srv://${mongo_username}:${mongo_password}@cluster0.0hjzj.mongodb.net/?retryWrites=true&w=majority`;
